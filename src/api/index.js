4d20686ef0988b533445a5050fa500d7178c92b8
Akramex <m_bennacer@estin.dz>
2024-05-04 22:53:15 +0100

    Comments management workflow

--- src/api/index.js
+++ src/api/index.js
@@ -5,11 +5,11 @@ const router = express.Router();
 const auth = require('./auth/auth.routes');
 const users = require('./users/users.routes');
 const posts = require('./posts/posts.routes');
-// const comments = require('./comments/comments.routes');
+const comments = require('./comments/comments.routes');
 
 router.use('/auth', auth);
 router.use('/users', users);
 router.use('/posts', posts);
-// router.use('/comments', comments);
+router.use('/comments', comments);
 
 module.exports = router;
